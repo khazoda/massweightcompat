514238b8d0a16e2d36c06ee2fde288dd1a455b53
khazoda <khazoda@gmail.com>
2026-06-01 17:49:11 +0100

    - moved from using modrinth-publish to posting to mr API directly. better for environment + deps

---
+++ publish.ps1
@@ -0,0 +1,61 @@
+param(
+    [Parameter(Mandatory)]
+    [string]$Version,
+
+    [string]$Changelog = ""
+)
+
+$ErrorActionPreference = "Stop"
+
+$headers = @{
+    Authorization = "Bearer $env:MODRINTH_TOKEN"
+    "User-Agent" = "khazoda/massweightcompat"
+}
+
+function Publish-Version {
+    param(
+        [string]$File,
+        [string[]]$Loaders
+    )
+
+    $data = @{
+        project_id = "bJLW7HUi"
+        version_number = $Version
+        version_title = $Version
+        version_body = $Changelog
+        release_channel = "release"
+        status = "listed"
+        loaders = $Loaders
+        fields = $fields
+        dependencies = $dependencies
+        file_parts = @("file")
+        primary_file = "file"
+    } | ConvertTo-Json -Depth 5 -Compress
+
+    Invoke-RestMethod `
+        -Method Post `
+        -Uri "https://api.modrinth.com/v3/version" `
+        -Headers $headers `
+        -Form @{
+            data = $data
+            file = Get-Item $File
+        } | Out-Null
+}
+
+$dependencies = @(
+    @{ project_id = "T9PomCSv"; dependency_type = "required" },
+    @{ project_id = "oWaK0Q19"; dependency_type = "optional" }
+)
+
+$fields = @{
+    game_versions = @("1.21.1")
+    environment = "server_only"
+}
+
+Publish-Version `
+    -File "export/massweightcompat-$Version.zip" `
+    -Loaders @("datapack")
+
+Publish-Version `
+    -File "export/massweightcompat-$Version.jar" `
+    -Loaders @("fabric", "neoforge")
